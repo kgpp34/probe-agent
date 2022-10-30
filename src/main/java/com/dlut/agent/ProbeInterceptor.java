b83cfd7da426d448c107de9e1d79f12881c4dede
YoungLH <974840768@qq.com>
2022-10-30 22:29:59 +0800

    fix: filter necessary files and update probe interceptor

--- src/main/java/com/dlut/agent/ProbeInterceptor.java
+++ src/main/java/com/dlut/agent/ProbeInterceptor.java
@@ -5,7 +5,7 @@ public class ProbeInterceptor {
     
     public static void intercept() throws Exception {
         System.out.println("[agent]:enter to agent probe service");
-        System.out.println("[agent]:ohhhhhhh");
+        System.out.println("[agent]:exec agent probe service");
         System.out.println("[agent]:exit agent probe service");   
     }
 }
